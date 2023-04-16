623cc84eb0f06a9bd11f0267f0afd29f58ff8f0c
Lilly <reubenh14@tutanota.com>
2023-04-16 16:52:18 +0200

    storage optimization: store address instead of ERC20

---
+++ src/interfaces/IERC20.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Unlicense
+interface IERC20 {
+    function balanceOf(address) external returns (uint256);
+
+    function transfer(address to, uint256 amount) external;
+}
